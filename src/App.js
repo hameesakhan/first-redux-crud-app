440247023a6b6317c45f5d67b03bf31ff1862e01
Hamees Ahmed Khan <hamees.a@allshorestaffing.com>
2022-07-06 23:53:10 +0500

    Basic setup fot RTK Redux

--- src/App.js
+++ src/App.js
@@ -1,55 +1,30 @@
-import React from 'react';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <Router>
+          <div className="App">
+            <Switch>
+              <Route exact path="/users" element={<UsersList />} />
+              <Route exact path="/users/:userId" element={<UserPage />} />
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { apiSlice } from '../features/api/apiSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware().concat(apiSlice.middleware))
 });
